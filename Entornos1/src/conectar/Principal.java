98f05b7ca946a17e9e2017777ba3f43bbd44d2c1
adminstrator <adminstrator@admin>
2018-05-01 23:21:59 +0200

    otro

--- Entornos1/src/conectar/Principal.java
+++ Entornos1/src/conectar/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA MUNDO NUEVO COMMIT");
 		System.out.println("otro syso nuevo");
+		System.out.println("y otro siso");
 	}
 
 }
