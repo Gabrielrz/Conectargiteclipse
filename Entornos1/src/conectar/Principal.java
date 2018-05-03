a6190fc8464c0fe766d6de40a429d1c1c43913cd
adminstrator <adminstrator@admin>
2018-05-03 13:24:49 +0200

    nuevo

--- Entornos1/src/conectar/Principal.java
+++ Entornos1/src/conectar/Principal.java
@@ -3,12 +3,7 @@ package conectar;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HOLA MUNDO NUEVO COMMIT");
-		System.out.println("otro syso nuevo");
-		System.out.println("y otro siso");
-		System.out.println("cambio nuevo para visualizar el pull 1");
-		System.out.println("este es una modificacion desde el userNuevo");
+		System.out.println("prueba de conflicto con nuevoUSer");
 	}
 
 }
