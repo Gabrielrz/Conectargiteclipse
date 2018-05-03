926d35390c469ef33696b13ac470fe6bb2996069
adminstrator <adminstrator@admin>
2018-05-03 14:16:27 +0200

    nuevo cambio

--- entornos3/src/entornos3/CompartirRepositorio1.java
+++ entornos3/src/entornos3/CompartirRepositorio1.java
@@ -3,7 +3,7 @@ package entornos3;
 public class CompartirRepositorio1 {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo ");
+		System.out.println(""+"hola mundo que tal ");
 		System.out.println("nueva modificaion e3 nueva");
 		System.out.println("prueba pull");
 		System.out.println("nueva prueba de pull");
