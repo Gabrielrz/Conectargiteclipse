0cf45e9c67e027a040bae789b70d52f812b76053
adminstrator <adminstrator@admin>
2018-05-03 02:14:37 +0200

    una nueva prueba de push

--- Entornos2/src/entornos2/CompartirRepositorio.java
+++ Entornos2/src/entornos2/CompartirRepositorio.java
@@ -3,7 +3,7 @@ package entornos2;
 public class CompartirRepositorio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("prueba de push");
 
 	}
 
