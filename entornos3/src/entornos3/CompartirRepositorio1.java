6856b4007eb66373dc9e2236121bd87bf79656cb
adminstrator <adminstrator@admin>
2018-05-03 13:54:06 +0200

    prueba pull

--- entornos3/src/entornos3/CompartirRepositorio1.java
+++ entornos3/src/entornos3/CompartirRepositorio1.java
@@ -5,6 +5,7 @@ public class CompartirRepositorio1 {
 	public static void main(String[] args) {
 		System.out.println("hola mundo ");
 		System.out.println("nueva modificaion e3 nueva");
+		System.out.println("prueba pull");
 	}
 
 }
