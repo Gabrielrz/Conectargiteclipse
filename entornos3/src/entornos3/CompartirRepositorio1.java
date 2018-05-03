42dddb4771c90cc8c4b2a340ffc127d718cacfde
adminstrator <adminstrator@admin>
2018-05-03 04:03:15 +0200

    he agregado un syso

--- entornos3/src/entornos3/CompartirRepositorio1.java
+++ entornos3/src/entornos3/CompartirRepositorio1.java
@@ -3,7 +3,7 @@ package entornos3;
 public class CompartirRepositorio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hola mundo ");
 
 	}
 
